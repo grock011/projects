b5ea4f1fad22ccd71437932a34ceb426936f61e6
grock011 <george.dav93@gmail.com>
2020-03-18 18:55:18 +0000

    Using switch

---
+++ Testing age using switch/src/Buzdugan.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+
+public class Buzdugan {
+	public static void main(String args[]) {
+		int age = 2;
+		
+		
+		switch(age) {
+		case 1 :
+			System.out.println("you are very young");
+			break;
+			
+		case 2 :
+			System.out.println("you are old");
+			break;
+			
+		default : 
+			System.out.println("I cannot verify your age");
+		}
+	}
+
+}
